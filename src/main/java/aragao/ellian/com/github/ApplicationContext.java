2e1cf728f796fc329c9a0722d6d6a42ddd8e963c
Ellian Aragão Dias <ellian.aragao@gmail.com>
2022-08-30 21:46:19 -0300

    feat: add application context dependency injection

---
+++ src/main/java/aragao/ellian/com/github/ApplicationContext.java
@@ -0,0 +1,48 @@
+package aragao.ellian.com.github;
+
+import aragao.ellian.com.github.core.usecase.impl.FindLowestRatedMoviesUsecaseImpl;
+import aragao.ellian.com.github.core.usecase.impl.SortMoviesUsecaseImpl;
+import aragao.ellian.com.github.infra.adapters.ImdbWorstMoviePort;
+import aragao.ellian.com.github.infra.crawler.CrawlerProcessor;
+import aragao.ellian.com.github.infra.crawler.ImdbDocumentConsumer;
+import aragao.ellian.com.github.infra.crawler.parser.ParserChartRatedMoviesPage;
+import aragao.ellian.com.github.infra.crawler.parser.ParserMovieBodyPage;
+import aragao.ellian.com.github.infra.crawler.parser.ParserReviewMovieBodyPage;
+import aragao.ellian.com.github.infra.crawler.router.RouterMovieChartConsumer;
+import aragao.ellian.com.github.infra.crawler.router.RouterMovieLinkDataAndReviews;
+
+import java.util.Objects;
+
+public abstract class ApplicationContext {
+
+    private static SortMoviesUsecaseImpl sortMoviesUsecase;
+    private static FindLowestRatedMoviesUsecaseImpl findLowestRatedMoviesUsecase;
+
+    public static void initializeContexts() {
+        final var parserChartRatedMoviesPage = new ParserChartRatedMoviesPage();
+        final var parserMovieBodyPage = new ParserMovieBodyPage();
+        final var parserReviewMovieBodyPage = new ParserReviewMovieBodyPage();
+        final var imdbDocumentConsumer = new ImdbDocumentConsumer("https://www.imdb.com");
+        final var routerMovieLinkDataAndReviews = new RouterMovieLinkDataAndReviews(parserReviewMovieBodyPage, parserMovieBodyPage, imdbDocumentConsumer);
+        final var routerMovieChartConsumer = new RouterMovieChartConsumer(routerMovieLinkDataAndReviews, parserChartRatedMoviesPage);
+        final var crawlerProcessor = new CrawlerProcessor(imdbDocumentConsumer, routerMovieChartConsumer);
+        final var imdbWorstMoviePort = new ImdbWorstMoviePort(crawlerProcessor);
+
+        sortMoviesUsecase = new SortMoviesUsecaseImpl();
+        findLowestRatedMoviesUsecase = new FindLowestRatedMoviesUsecaseImpl(imdbWorstMoviePort);
+    }
+
+    public static SortMoviesUsecaseImpl getSortMoviesUsecase() {
+        if (Objects.nonNull(sortMoviesUsecase)) {
+            return sortMoviesUsecase;
+        }
+        throw new RuntimeException("Context not initialized");
+    }
+
+    public static FindLowestRatedMoviesUsecaseImpl getFindLowestRatedMoviesUsecase() {
+        if (Objects.nonNull(findLowestRatedMoviesUsecase)) {
+            return findLowestRatedMoviesUsecase;
+        }
+        throw new RuntimeException("Context not initialized");
+    }
+}
